800d86d7c1afac342bec9b21b836d0d55456125c
Tuan Vu <kiemsitocnoi@gmail.com>
2018-02-18 09:54:12 +0700

    Update checkform

--- src/interact/CheckForm.java
+++ src/interact/CheckForm.java
@@ -39,7 +39,7 @@ public class CheckForm {
     public static boolean isWhiteSpace(String Value) {
         Pattern pattern = Pattern.compile("[\\w\\s]+");
         Matcher matcher = pattern.matcher(Value);
-        if (!matcher.find()) {
+        if (!matcher.find() || Value.indexOf(" ")!=-1) {
             return false;
         } else {
             return true;
@@ -48,7 +48,7 @@ public class CheckForm {
 
     //Check a string is numberic
     public static boolean isNumberic(String value) {
-        Pattern pattern = Pattern.compile("\\d+");
+        Pattern pattern = Pattern.compile("\\d+$");
         Matcher matcher = pattern.matcher(value);
         if (!matcher.find()) {
             return false;
@@ -88,7 +88,7 @@ public class CheckForm {
 
     //Validate phone number
     public static boolean checkPhoneNumber(String value) {
-        Pattern pattern = Pattern.compile("[0]{1}[1,9]{1}[0-9]{8,9}");
+        Pattern pattern = Pattern.compile("[0]{1}[1,9]{1}[0-9]{8,9}$");
         Matcher matcher = pattern.matcher(value);
         if (!matcher.find()) {
             return false;
